63e7e594490e88a19f65a01e36889a123405bb85
marckraw <marckraw@icloud.com>
2023-06-23 16:51:29 +0200

    feat: add habit tracker dummy component

---
+++ src/HabitTracker.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+
+const HabitTracker = () => {
+    return <div>This is habit tracker component</div>;
+};
+
+export { HabitTracker };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { TestClientComponent } from './TestClientComponent';
 export { TestServerComponent } from './TestServerComponent';
 export { Button } from './Button';
+export { HabitTracker } from './HabitTracker';
